e3a4043c139c6c6a4f43a45dbf023765b96daf02
MeetS <meets@dosepack.com>
2020-12-18 18:50:02 +0530

    :memo: Update configuration file

---
+++ config.py
@@ -0,0 +1,7 @@
+NEXT_NO_OF_DAYS = 7
+PERSONS_LIST = ['CYNTHIA MCMULLEN','MASON BARRETT']
+RESORT_LIST = ['PARK_CITY']
+EMAIL_ID = 'mason.pmb@gmail.com'
+PASSWORD = 'upworkTEST2020!'
+TIMEZONE = 'MST'
+WEB_LINK = 'https://www.epicpass.com/Plan-Your-Trip/Lift-Access/Reservations?reservation=true'
