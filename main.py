ed7e1b164ea53dd2348808b9155c3d11188214cf
MeetS <meets@dosepack.com>
2020-12-23 09:46:45 +0530

    Code modifications

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ while not complete and max_retries:
         pass_reservation = pass_reservation()
         if pass_reservation:
             print("successfully reserved passes")
-            complete = 1
+        complete = 1
     except Exception as e:
         print("Exception so Retrying...")
         max_retries -= 1
