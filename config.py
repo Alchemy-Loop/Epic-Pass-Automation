9503e4160e2f5f756d66c824ae57486d28bbed33
MeetS <meets@dosepack.com>
2020-12-28 10:58:57 +0530

    config file added

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 
 RESORT = "PARK CITY"
-EMAIL_ID = "mason.pmb@gmail.com"
-PASSWORD = "upworkTEST2020!"
+EMAIL_ID = "abc@gmail.com"
+PASSWORD = "password"
 NEXT_NO_OF_DAYS = 7
 PERSONS_LIST = ["CYNTHIA MCMULLEN","MASON BARRETT"]
